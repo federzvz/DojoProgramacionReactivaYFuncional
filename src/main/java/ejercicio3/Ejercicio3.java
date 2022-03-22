1d0aafd843a7618766151bc05fea9344df2ba499
federzvz <85709865+federzvz@users.noreply.github.com>
2022-03-22 15:35:14 -0300

    Ejercicio 3

--- src/main/java/ejercicio3/Ejercicio3.java
+++ src/main/java/ejercicio3/Ejercicio3.java
@@ -3,8 +3,23 @@ package ejercicio3;
 import java.util.Scanner;
 
 public class Ejercicio3 {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception{
+        int coeficiente, potencia,x,xc,xp;
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Derivar funciones de la forma ax^b \n");
+        do {
+            System.out.println("Escribe el coeficiente:");
+            coeficiente = teclado.nextInt();
+            System.out.println("Escribe la potencia:");
+            potencia = teclado.nextInt();
+            xc = coeficiente*potencia;
+            xp = potencia - 1;
+            System.out.println("La derivada es: "+xc+"x^"+xp+"\n");
+            System.out.println("Sacar otra derivada 1.si / 2.no");
+            x = teclado.nextInt();
+        }
+        while (x==1);
+            /*{
         Scanner sc=new Scanner(System.in);
         System.out.print("Ingresar expresion: ");
 
@@ -19,5 +34,6 @@ public class Ejercicio3 {
         double derivative[] = new double[C.length-1];
         for(int i=0;i<derivative.length;i++){
             derivative[i] = C[i]*(C.length - 1 -i );
-        }
+        }*/
+}
 }
